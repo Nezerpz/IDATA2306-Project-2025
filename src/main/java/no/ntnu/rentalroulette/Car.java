ec3839de906a6481aae4debf6351b5c386ff5eef
Henrik <henaa@stud.ntnu.no>
2025-02-10 08:20:06 +0100

    Creation of some default entries in the database, aswell as some more database entities

--- src/main/java/no/ntnu/rentalroulette/Car.java
+++ src/main/java/no/ntnu/rentalroulette/Car.java
@@ -8,98 +8,98 @@ import jakarta.persistence.Table;
 import jakarta.persistence.Id;
 
 /**
- * 
+ *
  */
 @Entity
 @Table(name = "car")
 public class Car {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
-	@Column(name = "car_model")
-    private String carModel;
-	@Column(name = "number_of_seats")
-    private int numberOfSeats;
-	@Column(name = "transmission_type")
-    private String transmissionType;
-	@Column(name = "fuel_type")
-    private String fuelType;
-	@Column(name = "year")
-    private int productionYear;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private int id;
+  @Column(name = "car_model")
+  private String carModel;
+  @Column(name = "number_of_seats")
+  private int numberOfSeats;
+  @Column(name = "transmission_type")
+  private String transmissionType;
+  @Column(name = "fuel_type")
+  private String fuelType;
+  @Column(name = "year")
+  private int productionYear;
 
-    public Car() {
-    }
+  public Car() {
+  }
 
-    public Car(
-            String model,
-            int seats,
-            String transmissionType,
-            String fuelType,
-            int productionYear) {
-        this.carModel = model;
-        this.numberOfSeats = seats;
-        this.transmissionType = transmissionType;
-        this.fuelType = fuelType;
-        this.productionYear = productionYear;
-    }
+  public Car(
+      String model,
+      int seats,
+      String transmissionType,
+      String fuelType,
+      int productionYear) {
+    this.carModel = model;
+    this.numberOfSeats = seats;
+    this.transmissionType = transmissionType;
+    this.fuelType = fuelType;
+    this.productionYear = productionYear;
+  }
 
-	/**
-     * Get Entity's row ID in Database.
-     *
-	 * @return id as string
-	 */
-	public int getId() {
-		return id;
-	}
-
-
-	/**
-     * Get Entity car model.
-     *
-	 * @return car model as string
-	 */
-	public String getCarModel() {
-		return carModel;
-	}
+  /**
+   * Get Entity's row ID in Database.
+   *
+   * @return id
+   */
+  public int getId() {
+    return id;
+  }
 
 
-	/**
-     * Get number of seats this car entity has
-     *
-	 * @return number of seats as int
-	 */
-	public int getNumberOfSeats() {
-		return numberOfSeats;
-	}
+  /**
+   * Get Entity car model.
+   *
+   * @return car model
+   */
+  public String getCarModel() {
+    return carModel;
+  }
 
 
-	/**
-     * Get car entity's transmission type.
-     *
-	 * @return transmission type as string
-	 */
-	public String getTransmissionType() {
-		return transmissionType;
-	}
+  /**
+   * Get number of seats this car entity has
+   *
+   * @return number of seats
+   */
+  public int getNumberOfSeats() {
+    return numberOfSeats;
+  }
 
 
-	/**
-     * Get car entity's fuel type.
-     *
-	 * @return fuel type as string
-	 */
-	public String getFuelType() {
-		return fuelType;
-	}
+  /**
+   * Get car entity's transmission type.
+   *
+   * @return transmission type
+   */
+  public String getTransmissionType() {
+    return transmissionType;
+  }
 
 
-	/**
-     * Get car entity's production year.
-     *
-	 * @return production year as string
-	 */
-	public int getProductionYear() {
-		return productionYear;
-	}
+  /**
+   * Get car entity's fuel type.
+   *
+   * @return fuel type
+   */
+  public String getFuelType() {
+    return fuelType;
+  }
+
+
+  /**
+   * Get car entity's production year.
+   *
+   * @return production year
+   */
+  public int getProductionYear() {
+    return productionYear;
+  }
 
 }
--- src/main/java/no/ntnu/rentalroulette/SessionUtil.java
+++ src/main/java/no/ntnu/rentalroulette/SessionUtil.java
@@ -14,66 +14,68 @@ import org.hibernate.Transaction;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+//TODO: Change to JPAREPOSITORY
+
 @Component
 public class SessionUtil {
-    private SessionFactory sessionFactory;
+  private SessionFactory sessionFactory;
 
-    @Autowired
-    public void setSessionFactory(EntityManagerFactory factory) {
-        this.sessionFactory = factory.unwrap(SessionFactory.class);
+  @Autowired
+  public void setSessionFactory(EntityManagerFactory factory) {
+    this.sessionFactory = factory.unwrap(SessionFactory.class);
+  }
+
+  public Session getSession() {
+    return sessionFactory.openSession();
+  }
+
+  public void closeSession(Session session) {
+    session.close();
+  }
+
+  public void closeSessionFactory() {
+    sessionFactory.close();
+  }
+
+  public void save(Object object) {
+    try (Session session = sessionFactory.openSession()) {
+      Transaction transaction = session.beginTransaction();
+      session.save(object);
+      transaction.commit();
     }
+  }
 
-    public Session getSession() {
-        return sessionFactory.openSession();
+  public <T> List<T> getAll(Class<T> clazz) {
+    try (Session session = sessionFactory.openSession()) {
+      CriteriaBuilder cb = session.getCriteriaBuilder();
+      CriteriaQuery<T> cq = cb.createQuery(clazz);
+      Root<T> rootEntry = cq.from(clazz);
+      CriteriaQuery<T> all = cq.select(rootEntry);
+
+      TypedQuery<T> allQuery = session.createQuery(all);
+      return allQuery.getResultList();
     }
+  }
 
-    public void closeSession(Session session) {
-        session.close();
+  public Object get(Class<?> clazz, int id) {
+    try (Session session = sessionFactory.openSession()) {
+      return session.get(clazz, id);
     }
+  }
 
-    public void closeSessionFactory() {
-        sessionFactory.close();
+  public void update(Object object) {
+    try (Session session = sessionFactory.openSession()) {
+      Transaction transaction = session.beginTransaction();
+      session.update(object);
+      transaction.commit();
     }
+  }
 
-    public void save(Object object) {
-        try (Session session = sessionFactory.openSession()) {
-            Transaction transaction = session.beginTransaction();
-            session.save(object);
-            transaction.commit();
-        }
+  public void delete(Object object) {
+    try (Session session = sessionFactory.openSession()) {
+      Transaction transaction = session.beginTransaction();
+      session.delete(object);
+      transaction.commit();
     }
-
-    public <T> List<T> getAll(Class<T> clazz) {
-        try (Session session = sessionFactory.openSession()) {
-            CriteriaBuilder cb = session.getCriteriaBuilder();
-            CriteriaQuery<T> cq = cb.createQuery(clazz);
-            Root<T> rootEntry = cq.from(clazz);
-            CriteriaQuery<T> all = cq.select(rootEntry);
-
-            TypedQuery<T> allQuery = session.createQuery(all);
-            return allQuery.getResultList();
-        }
-    }
-
-    public Object get(Class<?> clazz, int id) {
-        try (Session session = sessionFactory.openSession()) {
-            return session.get(clazz, id);
-        }
-    }
-
-    public void update(Object object) {
-        try (Session session = sessionFactory.openSession()) {
-            Transaction transaction = session.beginTransaction();
-            session.update(object);
-            transaction.commit();
-        }
-    }
-
-    public void delete(Object object) {
-        try (Session session = sessionFactory.openSession()) {
-            Transaction transaction = session.beginTransaction();
-            session.delete(object);
-            transaction.commit();
-        }
-    }
+  }
 }
