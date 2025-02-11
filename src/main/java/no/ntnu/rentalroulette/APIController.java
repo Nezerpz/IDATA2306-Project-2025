30a72a599536f570585ccd9ee7b80c6b973d0c30
ekroll <ekroll@pm.me>
2025-02-11 12:24:53 +0100

    add stuff from girtz example

--- src/main/java/no/ntnu/rentalroulette/APIController.java
+++ src/main/java/no/ntnu/rentalroulette/APIController.java
@@ -6,6 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 
 /**
  * Simple example controller
@@ -17,6 +21,16 @@ public class APIController {
     private SessionUtil sessionUtil;
 
     @GetMapping("/hello")
+    @Operation(
+        summary = "A simple test endpoint",
+        description = "Returns a small text"
+    )
+    @ApiResponses(value = {
+        @ApiResponse(
+            responseCode = "200",
+            description = "The small text returned in response body"
+        )
+    })
     public String greeting() {
         return "Sjalabais";
     }
