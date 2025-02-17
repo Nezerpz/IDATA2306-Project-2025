7ada03d8a6c997a5f2d9c54a09f4492b7300a1d9
ekroll <ekroll@pm.me>
2025-02-17 22:43:42 +0100

    beginnings of manufacturer endpoint for filters page

--- src/main/java/no/ntnu/rentalroulette/controller/CarController.java
+++ src/main/java/no/ntnu/rentalroulette/controller/CarController.java
@@ -23,4 +23,12 @@ public class CarController {
     List<Car> cars = new CopyOnWriteArrayList<>(carRepository.findAll());
     return new ResponseEntity<>(cars, HttpStatus.OK);
   }
+
+
+  @CrossOrigin(origins = "http://localhost:5173")
+  @GetMapping("/cars/models")
+  public ResponseEntity<List<String>> getCarModels() {
+    List<String> cars = new CopyOnWriteArrayList<String>(carRepository.findDistinctCarModel());
+    return new ResponseEntity<>(cars, HttpStatus.OK);
+  }
 }
