545b7c3125ac473bc9b29e9d574f5e2307e13a9f
Henrik <henaa@stud.ntnu.no>
2025-03-04 11:27:13 +0100

    Test made for frontend

--- src/main/java/no/ntnu/rentalroulette/controller/APIController.java
+++ src/main/java/no/ntnu/rentalroulette/controller/APIController.java
@@ -1,13 +1,13 @@
-package no.ntnu.rentalroulette;
+package no.ntnu.rentalroulette.controller;
 
-import java.util.ArrayList;
 import java.util.List;
+import no.ntnu.rentalroulette.entity.Order;
+import no.ntnu.rentalroulette.repository.OrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 
@@ -17,23 +17,39 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 @RestController
 public class APIController {
 
+  @Autowired
+  private OrderRepository orderRepository;
 
-    @Autowired
-    private SessionUtil sessionUtil;
+  @GetMapping("/hello")
+  @Operation(
+      summary = "A simple test endpoint",
+      description = "Returns a small text"
+  )
+  @ApiResponses(value = {
+      @ApiResponse(
+          responseCode = "200",
+          description = "The small text returned in response body"
+      )
+  })
+  public String greeting() {
+    return "Sjalabais";
+  }
 
-    @GetMapping("/hello")
-    @Operation(
-        summary = "A simple test endpoint",
-        description = "Returns a small text"
-    )
-    @ApiResponses(value = {
-        @ApiResponse(
-            responseCode = "200",
-            description = "The small text returned in response body"
-        )
-    })
-    public String greeting() {
-        return "Sjalabais";
-    }
+  @GetMapping("/orders")
+  @Operation(
+      summary = "Orders endpoint",
+      description = "Returns a list of all orders in the system"
+  )
+  @ApiResponses(value = {
+      @ApiResponse(
+          responseCode = "200",
+          description = "The list of orders returned in response body"
+      )
+  })
+  public ResponseEntity<List<Order>> orders() {
+    List<Order> orders = orderRepository.findAll();
+    return ResponseEntity.ok(orders);
+  }
+
 
 }
