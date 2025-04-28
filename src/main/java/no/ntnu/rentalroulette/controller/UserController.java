5e3cc66813bd5f7f415b90eee321bc68f86a71b8
Henrik <henaa@stud.ntnu.no>
2025-04-28 09:33:08 +0200

    Fixed endpoint typo

--- src/main/java/no/ntnu/rentalroulette/controller/UserController.java
+++ src/main/java/no/ntnu/rentalroulette/controller/UserController.java
@@ -48,7 +48,7 @@ public class UserController {
     }
   }
 
-  @GetMapping("users/self")
+  @GetMapping("/users/self")
   public ResponseEntity<User> getSelf(HttpServletRequest request) {
     User user = controllerUtil.getUserBasedOnJWT(request);
     User userToReturn = userRepository.findById(user.getId()).orElseThrow(
