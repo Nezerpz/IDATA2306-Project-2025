d2f72844a65976b9d374e1801cbbeeb682a3a729
ekroll <ekroll@pm.me>
2025-05-22 17:27:52 +0200

    add /api prefix for ReviewController

--- src/main/java/no/ntnu/rentalroulette/controller/ReviewController.java
+++ src/main/java/no/ntnu/rentalroulette/controller/ReviewController.java
@@ -16,8 +16,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
+@RequestMapping("/api")
 public class ReviewController {
 
   @Autowired
